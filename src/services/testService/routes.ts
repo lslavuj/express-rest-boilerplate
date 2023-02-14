1db6b60a87a5f8a49754812d3fd906b02615ca89
Luka Slavuj <lslavuj@dickeys.com>
2023-02-14 20:42:13 +0100

    fix: adds id on routes

--- src/services/testService/routes.ts
+++ src/services/testService/routes.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 
 import createTestController from './createTest/createTest.controller';
-import deleteTestController from './deleteTest/deleteTest.constroller';
-import getTestController from './getTest/getTest.constroller';
-import updateTestController from './updateTest/updateTest.constroller';
+import deleteTestController from './deleteTest/deleteTest.controller';
+import getTestController from './getTest/getTest.controller';
+import updateTestController from './updateTest/updateTest.controller';
 
 const router = express.Router();
 
 router.post('/test', createTestController);
-router.get('/test', getTestController);
-router.put('/test', updateTestController);
-router.delete('/test', deleteTestController);
+router.get('/test:id', getTestController);
+router.put('/test:id', updateTestController);
+router.delete('/test:id', deleteTestController);
 
 export default router;
