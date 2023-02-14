fbbe5f5ddb4e406946b547acec4fb28b7fbd495d
Luka Slavuj <lslavuj@dickeys.com>
2023-02-14 17:16:25 +0100

    refactor: changes server location

---
+++ bin/www/server.ts
@@ -0,0 +1,21 @@
+import chalk from 'chalk';
+import app from '../../src/app';
+
+const normalizePort = (val: number | string): number | string | boolean => {
+  const port: number = typeof val === 'string' ? parseInt(val, 10) : val;
+
+  if (Number.isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || '3000');
+
+app.listen(port, () => {
+  console.log(chalk.green(`Express server listening to port ${port}!`));
+});
