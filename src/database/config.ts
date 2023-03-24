f4c0c43a49008dd8ef3f4eccc4fbd0155440e72f
Luka Slavuj <lslavuj@dickeys.com>
2023-03-24 23:48:00 +0100

    feat: adds seeding functionality and initial seed

--- src/database/config.ts
+++ src/database/config.ts
@@ -2,11 +2,14 @@ import * as dotenv from 'dotenv';
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 
+import type { DataSourceOptions } from 'typeorm';
+import type { SeederOptions } from 'typeorm-extension';
+
 dotenv.config();
 
 const { DATABASE_HOST, DATABASE, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_USER } = process.env;
 
-const AppDataSource = new DataSource({
+const config: DataSourceOptions & SeederOptions = {
   type: 'mysql',
   host: DATABASE_HOST,
   port: Number(DATABASE_PORT),
@@ -15,8 +18,11 @@ const AppDataSource = new DataSource({
   database: DATABASE,
   entities: ['./src/database/models/**/*.ts'],
   migrations: ['./src/database/migrations/**/*.ts'],
+  seeds: ['./src/database/seeds/**/*.ts'],
   synchronize: true,
   logging: false,
-});
+};
+
+const AppDataSource = new DataSource(config);
 
 export default AppDataSource;
